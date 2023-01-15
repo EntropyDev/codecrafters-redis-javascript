4830f221bebce4d2d7b727bcf842aee5b18f9850
Vaibhav Chimalgi <vaibhavchimalgi@Vaibhavs-MacBook-Air.local>
2023-01-15 15:35:21 +0530

    write PONG

--- app/main.js
+++ app/main.js
@@ -6,6 +6,14 @@ console.log("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
   // Handle connection
+//   return "+PONG\r\n";
+// console.log('client connected');
+  connection.on('end', () => {
+    console.log('client disconnected');
+  });
+  connection.write('PONG\r\n');
+  connection.pipe(connection);
+// console.log(connection);
 });
 
 server.listen(6379, "127.0.0.1");
